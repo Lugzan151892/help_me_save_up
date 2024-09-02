d7f81959831afd9fc4ca87c992c2019291e9c7e0
Denis Gukov <denis.guk@konomic.com>
2024-09-02 20:47:00 +0300

    auth: fixed test redux store

--- src/services/reducers/test.ts
+++ src/services/reducers/test.ts
@@ -1,22 +1,22 @@
-import { createSlice } from "@reduxjs/toolkit"
+import { createSlice } from "@reduxjs/toolkit";
 
-const initialState: {user: null|string, testData: string } = {
-  user: null,
-  testData: 'test'
-}
+const initialState: { user: null | string; testData: string } = {
+	user: null,
+	testData: "test",
+};
 
 export const testSlice = createSlice({
-  name: 'test',
-  initialState,
-  reducers: {
-    user: state => {
-      state.user = 'Denis'
-    },
-    test: state => {
-      state.testData = 'changed test'
-    }
-  }
-})
+	name: "test",
+	initialState,
+	reducers: {
+		user(state) {
+			state.user = "Denis";
+		},
+		test(state) {
+			state.testData = "changed test";
+		},
+	},
+});
 
 export const { user, test } = testSlice.actions;
 
